263d64e0eec9eacbdc3b3e2cf775fe0f0ff5d403
LuigiM <jlpadilla@innovacion.gob.sv>
2025-08-13 15:37:56 -0600

    Style: Correcciones en responsive

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
 interface CardInfoProps {
+  id?: number;
   image?: string;
   title?: string;
   gender?: string;
@@ -7,6 +11,7 @@ interface CardInfoProps {
 }
 
 export const CardInfo: React.FC<CardInfoProps> = ({
+  id,
   image,
   title,
   gender,
@@ -14,20 +19,37 @@ export const CardInfo: React.FC<CardInfoProps> = ({
   size = "small",
 }) => {
   const formatYear = year ? year.slice(0, 4) : "";
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
-    <div className={size === 'large' ? 'flex flex-col' : 'flex-shrink-0 w-40'}>
-      <div className="aspect-[2/3] w-full overflow-hidden rounded-lg">
+    <div
+      className={`${
+        size === "large" ? "flex flex-col mx-auto w-[20rem] sm:w-full" : "flex-shrink-0 w-40"
+      } group relative`}
+    >
+      <Link
+        to={`/movie/${id}`}
+        className="block aspect-[2/3] w-full overflow-hidden rounded-lg relative group-hover:scale-105 transition-transform duration-300"
+      >
         <img
           src={`https://image.tmdb.org/t/p/w500${image}`}
           alt={title}
           className="w-full h-full object-cover block"
         />
-      </div>
+
+        <div className="absolute inset-0 bg-black/25 opacity-0 group-hover:opacity-100 transition-opacity"></div>
+      </Link>
+
+      <button
+        onClick={() => setIsFavorite(!isFavorite)}
+        className="absolute top-2 right-2 bg-black/60 hover:bg-black p-2 rounded-full text-white z-10"
+      >
+        <span className={isFavorite ? "text-red-500" : ""}>♥</span>
+      </button>
 
       <h2 className="mt-2 font-semibold truncate">{title}</h2>
       <p className="text-sm text-gray-400">
-        {formatYear}-{gender}
+        {formatYear} - {gender}
       </p>
     </div>
   );
