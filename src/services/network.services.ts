39fece4690b582e5417452bb7827b7e7ac6606a4
LuigiM <jlpadilla@innovacion.gob.sv>
2025-08-13 08:49:11 -0600

    Fix(Network services): Configuracion de network para peticiones

--- src/services/network.services.ts
+++ src/services/network.services.ts
@@ -1,37 +1,38 @@
-export default async function Config(method: string, url: string, body = {}) {
-    const app_url = import.meta.env.VITE_APP_URL;
-    const api_key = import.meta.env.VITE_API_KEY;
+export default async function Config(
+  method: string,
+  url: string,
+  query?: Record<string, any>,
+  body?: Record<string, any>
+) {
+  const app_url = import.meta.env.VITE_APP_URL;
+  const api_key = import.meta.env.VITE_API_KEY;
 
-    const config = () => {
-        if (method === 'GET') {
-            return {
-                method: method
-            }
-        }
-        return {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body)
-        }
+  const params = new URLSearchParams({
+    api_key,
+    language: "es-ES",
+    ...(query || {}),
+  });
+
+  const config: RequestInit = {
+    method: method.toUpperCase(),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (method.toUpperCase() !== "GET" && body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    const api = await fetch(`${app_url}${url}?${params.toString()}`, config);
+
+    if (!api.ok) {
+      throw new Error(`HTTP error! Status: ${api.status}`);
     }
-
-    try{
-
-        const api = await fetch(`${app_url}${url}?api_key=${api_key}&language=es-ES`, config())
-
-        if (!api.ok) { 
-            throw new Error(`HTTP error! Status: ${api.status}`);
-        }
-        const res = await api.json();
-        return res;
-
-    }catch(error){
-
-        console.log(error);
-        return {}
-
-    }
-   
+    return await api.json();
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 }
