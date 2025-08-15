98bcc4742a4cd44c2660706e3f5a44594cc59a5a
LuigiM <jlpadilla@innovacion.gob.sv>
2025-08-15 09:21:39 -0600

    Fix(Error page): Añadir mensaje

--- src/views/ErrorSystems/ErrorPage.tsx
+++ src/views/ErrorSystems/ErrorPage.tsx
@@ -9,6 +9,7 @@ export const ErrorPage = () => {
     <div className="flex flex-col items-center justify-center gap-4 text-white h-screen">
       <h1 className="text-h1">¡Oops! Algo salió mal</h1>
       {/* <p>{error?.statusText || error?.message || "Error desconocido"}</p> */}
+      <p>Error al cargar, intenta de nuevo</p>
       <Link to="/">
         <Button className="btn-primary" variant="contained">
           Volver al inicio
